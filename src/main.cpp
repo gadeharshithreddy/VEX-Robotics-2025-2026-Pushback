c707d4d3fd580cf5c65e579ae6dfa64bd04cc71e
gadeharshithreddy <gadeharshithreddy@gmail.com>
2025-12-19 12:50:03 -0600

    Added Lemlib

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,32 @@
 #include "main.h"
+#include "pros/adi.hpp"
+#include "pros/imu.hpp"
+#include "pros/motor_group.hpp"
+#include "pros/optical.hpp"
+#include "lemlib/api.hpp"
+
+// Brain Devices
+
+// Controller
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+
+// Drivetrain Motors
+pros::MotorGroup leftMotors({});
+pros::MotorGroup rightMotors({});
+
+// Intake Motors
+pros::Motor topIntakeMotor();
+pros::Motor bottomIntakeMotor();
+
+// Optical Sensor
+pros::Optical colorSensor();
+
+// Pneumatics
+pros::adi::Pneumatics middleGoalMech();
+pros::adi::Pneumatics loaderMech();
+
+// IMU
+pros::Imu imu();
 
 /**
  * A callback function for LLEMU's center button.
@@ -74,7 +102,7 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
+	
 	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
 	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
 
