bf2a041feda299a347a278ca2da0e3d90b2c1b39
gadeharshithreddy <gadeharshithreddy@gmail.com>
2025-12-20 20:39:48 -0600

    Added Driver Control

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,119 @@
 #include "main.h"
+#include "lemlib/chassis/chassis.hpp"
+#include "lemlib/chassis/trackingWheel.hpp"
+#include "pros/abstract_motor.hpp"
 #include "pros/adi.hpp"
 #include "pros/imu.hpp"
 #include "pros/motor_group.hpp"
 #include "pros/optical.hpp"
 #include "lemlib/api.hpp"
+#include "pros/rotation.hpp"
 
 // Brain Devices
 
 // Controller
-pros::Controller master(pros::E_CONTROLLER_MASTER);
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 // Drivetrain Motors
-pros::MotorGroup leftMotors({});
-pros::MotorGroup rightMotors({});
+pros::MotorGroup left_motors({-11, -1, -2}, pros::MotorGearset::blue);
+pros::MotorGroup right_motors({7, 8, 20}, pros::MotorGearset::blue);
 
 // Intake Motors
-pros::Motor topIntakeMotor();
-pros::Motor bottomIntakeMotor();
+pros::Motor top_intake_motor(-3);
+pros::Motor bottom_intake_motor(10);
 
 // Optical Sensor
-pros::Optical colorSensor();
+pros::Optical color_sensor(19);
 
 // Pneumatics
-pros::adi::Pneumatics middleGoalMech();
-pros::adi::Pneumatics loaderMech();
+pros::adi::Pneumatics middle_goal_mech('A', false);
+pros::adi::Pneumatics loader_mech('B', true);
 
-// IMU
-pros::Imu imu();
+// IMU + Rotation Sensors
+pros::Imu imu(5);
+pros::Rotation horizontal_sensor(6);
+pros::Rotation vertical_sensor(9);
 
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
-}
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
+
+// Lemlib Drivetrain Setup
+lemlib::Drivetrain drivetrain(
+	&left_motors,
+	&right_motors,
+	12.08,
+	lemlib::Omniwheel::OLD_325,
+	450,
+	8
+);
+
+// Lemlib Tracking Wheel Setup
+lemlib::TrackingWheel horizontal_tracking_wheel(
+	&horizontal_sensor,
+	lemlib::Omniwheel::NEW_275,
+	0
+);
+lemlib::TrackingWheel vertical_tracking_wheel(
+	&vertical_sensor,
+	lemlib::Omniwheel::NEW_275,
+	0
+);
+
+// Lemlib Odometry Configuration
+lemlib::OdomSensors sensors(
+	&vertical_tracking_wheel,
+	nullptr,
+	&horizontal_tracking_wheel,
+	nullptr,
+	&imu	
+);
+
+// Lemlib PID Tuning
+lemlib::ControllerSettings lateral_controller(
+	10, // proportional gain (kP)
+	0, // integral gain (kI)
+	3, // derivative gain (kD)
+	0, // anti windup
+	0, // small error range, in inches
+	0, // small error range timeout, in milliseconds
+	0, // large error range, in inches
+	0, // large error range timeout, in milliseconds
+	0 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(
+	2, // proportional gain (kP)
+    0, // integral gain (kI)
+    10, // derivative gain (kD)
+	0, // anti windup
+	0, // small error range, in degrees
+	0, // small error range timeout, in milliseconds
+	0, // large error range, in degrees
+	0, // large error range timeout, in milliseconds
+	0 // maximum acceleration (slew)
+);
+
+// Controller Input Changing
+// lemlib::ExpoDriveCurve throttle_curve(3, // joystick deadband out of 127
+//                                      10, // minimum output where drivetrain will move out of 127
+//                                      1.019 // expo curve gain
+// );
+
+// // input curve for steer input during driver control
+// lemlib::ExpoDriveCurve steer_curve(3, // joystick deadband out of 127
+//                                   10, // minimum output where drivetrain will move out of 127
+//                                   1.019 // expo curve gain
+// );
+
+// Lemlib Chassis
+lemlib::Chassis chassis(
+	drivetrain,
+	lateral_controller,
+	angular_controller,
+	sensors
+);
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -51,10 +122,19 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
+	pros::lcd::initialize(); // initialize brain screen
+    chassis.calibrate(); // calibrate sensors
+    // print position to brain screen
+    pros::Task screen_task([&]() {
+        while (true) {
+            // print robot location to the brain screen
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            // delay to save resources
+            pros::delay(20);
+        }
+    });
 }
 
 /**
@@ -102,21 +182,16 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+	// loop forever
+    while (true) {
+        // get left y and right x positions
+        int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
 
+        // move the robot
+        chassis.arcade(leftY, rightX);
 
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
-
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
-	}
+        // delay to save resources
+        pros::delay(25);
+    }
 }
